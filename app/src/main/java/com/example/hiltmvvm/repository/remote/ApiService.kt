2c11bf9cda50ee2afd395a92fc6bdbb45d287b9c
Ajay Thakurathi <ajay.thakurathi@id123.io>
2023-05-10 18:33:47 +0530

    rest api code

--- app/src/main/java/com/example/hiltmvvm/repository/remote/ApiService.kt
+++ app/src/main/java/com/example/hiltmvvm/repository/remote/ApiService.kt
@@ -1,5 +1,10 @@
 package com.example.hiltmvvm.repository.remote
 
+import com.example.hiltmvvm.model.PostServiceObject
+import retrofit2.http.GET
+
 interface ApiService {
 
+    @GET("posts")
+    suspend fun getPosts(): PostServiceObject
 }
