607ab312e4c701922aeeb95bd4026ccc431542aa
Ajay Singh Thakurathi <ajayst53@gmail.com>
2023-05-10 23:30:22 +0530

    ongoing retrofit part

--- app/src/main/java/com/example/hiltmvvm/repository/MainRepository.kt
+++ app/src/main/java/com/example/hiltmvvm/repository/MainRepository.kt
@@ -1,20 +1,49 @@
 package com.example.hiltmvvm.repository
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import com.example.hiltmvvm.model.Post
 import com.example.hiltmvvm.model.User
 import com.example.hiltmvvm.repository.local.UserDao
+import com.example.hiltmvvm.repository.remote.ApiService
+import kotlinx.coroutines.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MainRepository @Inject constructor() {
 
+    private var job: CompletableJob? = null
+
+    //local
     @Inject
     lateinit var userDao: UserDao
 
     suspend fun insertUser(user: User) {
         userDao.insert(user)
         Log.d(TAG, "insertUser: $user")
+    }
+
+    //remote
+    @Inject
+    lateinit var apiService: ApiService
+
+    suspend fun getPostById(id: Long): LiveData<Post> {
+        job = Job()
+        return object : LiveData<Post>() {
+            override fun onActive() {
+                super.onActive()
+                job?.let {
+                    CoroutineScope(Dispatchers.IO + it).launch {
+                        apiService.getPostById(id)
+                    }
+                }
+            }
+        }
+    }
+
+    fun cancelJobs() {
+        job?.cancel()
     }
 
     companion object {
--- app/src/main/java/com/example/hiltmvvm/repository/remote/ApiService.kt
+++ app/src/main/java/com/example/hiltmvvm/repository/remote/ApiService.kt
@@ -1,10 +1,15 @@
 package com.example.hiltmvvm.repository.remote
 
+import com.example.hiltmvvm.model.Post
 import com.example.hiltmvvm.model.PostServiceObject
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("posts")
     suspend fun getPosts(): PostServiceObject
+
+    @GET("posts/{Id}")
+    suspend fun getPostById(@Path("Id") id: Long): Post
 }
