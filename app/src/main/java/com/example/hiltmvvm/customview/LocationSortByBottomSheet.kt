6122df0b48ca42c8d2a077bc21df5d326077e4b2
Ajay Singh Thakurathi <ajayst53@gmail.com>
2023-09-26 23:41:27 +0530

    Added Radio buttoons

--- app/src/main/java/com/example/hiltmvvm/customview/LocationSortByBottomSheet.kt
+++ app/src/main/java/com/example/hiltmvvm/customview/LocationSortByBottomSheet.kt
@@ -1,13 +1,13 @@
 package com.example.hiltmvvm.customview
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import com.example.hiltmvvm.R
 import com.example.hiltmvvm.databinding.BottomSheetLocationSortbyBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.example.hiltmvvm.util.Enum
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class LocationSortByBottomSheet : BottomSheetDialogFragment() {
@@ -15,23 +15,55 @@ class LocationSortByBottomSheet : BottomSheetDialogFragment() {
     private var _binding: BottomSheetLocationSortbyBinding? = null
     private val binding get() = _binding!!
 
+    private var selectedSort: String? = null
+    private var appliedSort: String? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        setStyle(STYLE_NORMAL, R.style.MyBottomSheet)
+        return super.onCreateDialog(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = BottomSheetLocationSortbyBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //init UI
-        //set onclick listener
-        binding.tv1.setOnClickListener {
-            Toast.makeText(requireContext(), "Text 1", Toast.LENGTH_SHORT).show()
+        binding.btnApply.setOnClickListener{
+
         }
 
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                binding.rbDistance.id -> {
+                    selectedSort = Enum.SortBy.DISTANCE.type
+                }
+
+                binding.rbRating.id -> {
+                    selectedSort = Enum.SortBy.RATING.type
+                }
+
+                binding.rbReviewCount.id -> {
+                    selectedSort = Enum.SortBy.REVIEW_COUNT.type
+                }
+
+                binding.rbBestMatch.id -> {
+                    selectedSort = Enum.SortBy.BEST_MATCH.type
+                }
+            }
+        }
+
+
+
+        binding.btnClose.setOnClickListener {
+            dismiss()
+        }
     }
 }
